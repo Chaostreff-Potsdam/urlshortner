98917e95a9d2b214aaae81ad0b4d50b625d90fdd
Sven Koehler <sven.koehler@hpi.de>
2025-04-17 16:41:59 +0200

    migration path for longer urls

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config.from_object('settings')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 csrf = CSRFProtect(app)
 bcrypt = Bcrypt(app)
